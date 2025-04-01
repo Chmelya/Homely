0ac1866cca9d07aa154636b7b9c05013bc0a885c
chmelya <72091370+Chmelya@users.noreply.github.com>
2025-04-01 15:36:59 +0300

    Refactor rbac seed

---
+++ Homely-backend/Homely.Data/Configurations/Rbac/Enities/RolePermissionEnities.cs
@@ -0,0 +1,26 @@
+﻿using Homely.Domain.Entities.Rbac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homely.Infrastructure.Data.Configurations.Rbac.Enities;
+
+internal static class RolePermissionEnities
+{
+    private static List<(int, List<Permission>)> RolesPermisssions = new()
+    {
+        (RoleEntities.Admin.Id, PermissionEntities.AdminPermissions),
+        (RoleEntities.Resident.Id, PermissionEntities.ResidentPermissions),
+    };
+
+    public static List<object> GetRolePermissonRelationsEnities()
+    {
+        return RolesPermisssions
+            .SelectMany(
+                r => r.Item2,
+                (r, permission) => new { RolesId = r.Item1, PermissionsId = permission.Id })
+            .ToList<object>();
+    }
+}
