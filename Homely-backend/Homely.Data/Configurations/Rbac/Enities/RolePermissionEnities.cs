e65aeadf63391aae7e4113d7318a730fc9c3b009
chmelya <72091370+Chmelya@users.noreply.github.com>
2025-04-01 17:03:51 +0300

    Refactor startup

--- Homely-backend/Homely.Data/Configurations/Rbac/Enities/RolePermissionEnities.cs
+++ Homely-backend/Homely.Data/Configurations/Rbac/Enities/RolePermissionEnities.cs
@@ -1,9 +1,4 @@
 ﻿using Homely.Domain.Entities.Rbac;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homely.Infrastructure.Data.Configurations.Rbac.Enities;
 
---
+++ Homely-backend/Homely.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Homely.Infrastructure.Identification.Authentication;
+using Homely.Infrastructure.Identification.Authentication.Interfaces;
+using Homely.Infrastructure.Identification.Authorization;
+using Homely.Infrastructure.Identification.OptionsSetup;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Homely.Infrastructure.Identification
+{
+    public static class DependencyInjection
+    {
+        public static void AddJwt(this IServiceCollection services)
+        {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer();
+
+            services.ConfigureOptions<JwtOptionsSetup>();
+            services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+            services.AddScoped<IJwtProvider, JwtProvider>();
+        }
+
+        public static void UseJwt(this WebApplication app)
+        {
+            app.UseMiddleware<AuthorizationMiddleware>();
+        }
+    }
+}
