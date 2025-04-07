64b1a52f427a11b67640cd44a0f64c718a362ab7
chmelya <72091370+Chmelya@users.noreply.github.com>
2025-04-07 12:43:18 +0300

    requests list ui

--- homely-frontend/app/pages/requests/requests-main/requests-main.tsx
+++ homely-frontend/app/pages/requests/requests-main/requests-main.tsx
@@ -9,6 +9,7 @@ import {
 	TableContainer,
 	TableHead,
 	TableRow,
+	Tooltip,
 	Typography,
 } from '@mui/material';
 import CriticalIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
@@ -21,6 +22,9 @@ import type { Route } from './+types/requests-main';
 import type { ServiceRequest } from '~/models/service-request';
 import { UrgencyEnum } from '~/models/urgency';
 import { CategoryEnum } from '~/models/categories';
+import { RequestStatuses, Urgencies, type Dictionary } from '~/models/pairs';
+import type React from 'react';
+import { useMemo, type ReactElement, type ReactNode } from 'react';
 
 export async function clientLoader() {
 	const request = await RequestsService.getPagedRequests(1, 10);
@@ -34,6 +38,17 @@ const RequestsMainPage = ({ loaderData }: Route.ComponentProps) => {
 
 	const requests = loaderData.items as ServiceRequest[];
 
+	const UrgencyIcons = useMemo(() => {
+		const urgencyIcons: Dictionary<ReactElement> = {};
+		urgencyIcons['1'] = <CriticalIcon />;
+		urgencyIcons['2'] = <HighIcon />;
+		urgencyIcons['3'] = <MediumIcon />;
+		urgencyIcons['4'] = <LowIcon />;
+		urgencyIcons['5'] = <LowestIcon />;
+
+		return urgencyIcons;
+	}, []);
+
 	return (
 		<Box>
 			<Typography className='' variant='h4'>
@@ -42,7 +57,7 @@ const RequestsMainPage = ({ loaderData }: Route.ComponentProps) => {
 
 			<TableContainer component={Paper} elevation={3} className='px-2.5'>
 				<Table>
-					<TableHead>
+					<TableHead className='bg-pink-400'>
 						<TableRow>
 							<TableCell>Title</TableCell>
 							<TableCell align='center'>Status</TableCell>
@@ -55,9 +70,13 @@ const RequestsMainPage = ({ loaderData }: Route.ComponentProps) => {
 							<TableRow>
 								<TableCell>{req.title}</TableCell>
 								<TableCell align='center'>
-									<Chip label={req.status} color='default' />
+									<Chip label={RequestStatuses[req.status]} color='default' />
 								</TableCell>
-								<TableCell align='center'>{UrgencyEnum[req.urgency]}</TableCell>
+								<TableCell align='center'>
+									<Tooltip title={Urgencies[req.urgency]} placement='right'>
+										{UrgencyIcons[req.urgency]}
+									</Tooltip>
+								</TableCell>
 								<TableCell align='right'>
 									{CategoryEnum[req.category]}
 								</TableCell>
