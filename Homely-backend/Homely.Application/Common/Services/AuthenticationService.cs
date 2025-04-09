430068ac84afa36aa7d8090d8b95474807723576
chmelya <72091370+Chmelya@users.noreply.github.com>
2025-04-09 20:18:29 +0300

    reqorganize auth

---
+++ Homely-backend/Homely.Application/Common/Services/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using ErrorOr;
+using Homely.Application.Common.Interfaces.Repositories;
+using Homely.Application.Common.Interfaces.Services;
+using Homely.Infrastructure.Identification.Authentication.Interfaces;
+
+namespace Homely.Application.Common.Services;
+
+public class AuthenticationService(
+    IJwtProvider tokenProvider,
+    IUserRepository userRepository) : IAuthenticationService
+{
+    public async Task<ErrorOr<string>> SignIn(string email, string password, CancellationToken cancellationToken = default)
+    {
+        var user = await userRepository.GetWithRole(u => u.Email == email, cancellationToken);
+
+        if (user?.Password != password)
+        {
+            return Error.NotFound(description: "Login or password is incorrect");
+        }
+
+        var token = await tokenProvider.GenerateJwtToken(user);
+
+        return token;
+    }
+}
