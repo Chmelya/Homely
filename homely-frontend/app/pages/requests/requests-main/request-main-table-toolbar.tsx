1075b5589412f6c862b0dd33f336f10d1398d164
chmelya <72091370+Chmelya@users.noreply.github.com>
2025-04-11 14:13:10 +0300

    filter menu implmentation

--- homely-frontend/app/pages/requests/requests-main/request-main-table-toolbar.tsx
+++ homely-frontend/app/pages/requests/requests-main/request-main-table-toolbar.tsx
@@ -1,23 +1,47 @@
 import {
 	Box,
-	Checkbox,
+	Button,
+	FormControl,
 	IconButton,
+	InputLabel,
 	ListItemText,
 	Menu,
 	MenuItem,
-	Paper,
 	Select,
 	Stack,
 	Toolbar,
 	Tooltip,
 	Typography,
+	type SelectChangeEvent,
 } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import type { ServiceRequestOptions } from '~/models/serviceRequestOptions';
 import { useState } from 'react';
+import type { DropdownValue } from '~/models/dropdownValue';
+import { useNavigate } from 'react-router';
+import { ROUTES } from '~/routes/paths';
 
+// TODO: Menu to separate component
 const TableToolbar = ({ options }: { options: ServiceRequestOptions }) => {
-	const [isMenuOpened, setIsMenuOpened] = useState(false);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const isOpened = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const navigate = useNavigate();
+
+	const applyHandler = () => {
+		handleClose();
+	};
+
+	const resetHandler = () => {
+		handleClose();
+		navigate(ROUTES.requestsMainDefault());
+	};
 
 	return (
 		<Toolbar className='bg-pink-400'>
@@ -30,92 +54,73 @@ const TableToolbar = ({ options }: { options: ServiceRequestOptions }) => {
 				<Typography className='' variant='h5'>
 					My Requests
 				</Typography>
-				<Tooltip title='Filter list'>
-					<IconButton onClick={() => setIsMenuOpened(true)}>
-						<Menu open={isMenuOpened}>
-							{/* <MenuItem>
-								<ListItemText>Categories</ListItemText>
-							</MenuItem>
-							<MenuItem>
-								<ListItemText>Urgencies</ListItemText>
-							</MenuItem>
-							<MenuItem>
-								<ListItemText>Statuses</ListItemText>
-							</MenuItem> */}
-							<Paper className='p-4 min-w-120'>
-								<Stack direction='column' gap={2}>
-									<Stack direction='row' alignItems='center' gap={2}>
-										<ListItemText className='min-w-4'>Categories:</ListItemText>
-										<Select
-											labelId='demo-multiple-checkbox-label'
-											id='demo-multiple-checkbox'
-											fullWidth
-											//multiple
-											// value={personName}
-											// onChange={handleChange}
-											// input={<OutlinedInput label='Tag' />}
-											// renderValue={(selected) => selected.join(', ')}
-											// MenuProps={MenuProps}
-										>
-											{options.categories.map((c) => (
-												<MenuItem key={c.key} value={c.key}>
-													<Checkbox />
-													<ListItemText>{c.value}</ListItemText>
-												</MenuItem>
-											))}
-										</Select>
-									</Stack>
-									<Stack direction='row' alignItems='center'>
-										<ListItemText>Urgencies: </ListItemText>
-										<Select
-											labelId='demo-multiple-checkbox-label'
-											id='demo-multiple-checkbox'
-											fullWidth
-											//multiple
-											// value={personName}
-											// onChange={handleChange}
-											// input={<OutlinedInput label='Tag' />}
-											// renderValue={(selected) => selected.join(', ')}
-											// MenuProps={MenuProps}
-										>
-											{options.urgencies.map((u) => (
-												<MenuItem key={u.key} value={u.key}>
-													<Checkbox />
-													<ListItemText>{u.value}</ListItemText>
-												</MenuItem>
-											))}
-										</Select>
-									</Stack>
-									<Stack direction='row' alignItems='center'>
-										<ListItemText>Statuses: </ListItemText>
-										<Select
-											labelId='demo-multiple-checkbox-label'
-											id='demo-multiple-checkbox'
-											fullWidth
-											//multiple
-											// value={personName}
-											// onChange={handleChange}
-											// input={<OutlinedInput label='Tag' />}
-											// renderValue={(selected) => selected.join(', ')}
-											// MenuProps={MenuProps}
-										>
-											{options.statuses.map((s) => (
-												<MenuItem key={s.key} value={s.key}>
-													<Checkbox />
-													<ListItemText>{s.value}</ListItemText>
-												</MenuItem>
-											))}
-										</Select>
-									</Stack>
-								</Stack>
-							</Paper>
-						</Menu>
+				<Tooltip title={'Filter list'}>
+					<IconButton onClick={handleClick}>
 						<FilterListIcon />
 					</IconButton>
 				</Tooltip>
+				<Menu open={isOpened} anchorEl={anchorEl} onClose={handleClose}>
+					<Box className='p-4 min-w-120'>
+						<Stack direction='column' gap={2}>
+							<MenuSelect items={options.statuses} label='Statuses' />
+							<MenuSelect items={options.categories} label='Categories' />
+							<MenuSelect items={options.urgencies} label='Urgencies' />
+						</Stack>
+						<Stack className='mt-4' direction='row' gap={2}>
+							<Button onClick={resetHandler} fullWidth variant='outlined'>
+								Reset
+							</Button>
+							<Button onClick={applyHandler} fullWidth variant='contained'>
+								Apply
+							</Button>
+						</Stack>
+					</Box>
+				</Menu>
 			</Stack>
 		</Toolbar>
 	);
 };
 
 export default TableToolbar;
+
+const MenuSelect = ({
+	items,
+	label,
+}: {
+	items: DropdownValue[];
+	label: string;
+}) => {
+	const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+	const handleChange = (event: SelectChangeEvent<typeof selectedItems>) => {
+		const {
+			target: { value },
+		} = event;
+		setSelectedItems(typeof value === 'string' ? value.split(',') : value);
+	};
+
+	return (
+		<Stack direction='row' alignItems='center'>
+			<FormControl fullWidth>
+				<InputLabel id={`${label}-input-label`}>{label}</InputLabel>
+				<Select
+					labelId={`${label}-multiply-select-label`}
+					id={`${label}-multiply-select`}
+					size='small'
+					fullWidth
+					multiple
+					value={selectedItems}
+					onChange={handleChange}
+					renderValue={(selected) => selected.join(', ')}
+				>
+					{items.map((item) => (
+						<MenuItem key={item.key} value={item.value}>
+							{/* <Checkbox /> */}
+							<ListItemText>{item.value}</ListItemText>
+						</MenuItem>
+					))}
+				</Select>
+			</FormControl>
+		</Stack>
+	);
+};
